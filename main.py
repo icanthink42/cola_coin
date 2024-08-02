dbe82fe25903e9a0457099ecea0c686067295dd8
Jack <neelemanjack@gmail.com>
2024-08-02 12:05:44 -0700

    Commented out all company related commands

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import ui
-from pandas import DataFrame
+# import ui
+# from pandas import DataFrame
 import discord
 import api
 from discord.commands import Option
@@ -43,40 +43,40 @@ async def pay(ctx, user: discord.User, amount: float):
     else:
         await ctx.respond(error_message, ephemeral=True)
 
-@bot.slash_command(guild_ids=guild_ids)
-async def create_company(ctx, name: str, shares: int):
-    error_message = await api.create_company(ctx.user.id, name, shares)
-    if error_message is None:
-        await ctx.respond(f"{name} has been created and {shares} shares have been issued.", ephemeral=True)
-    else:
-        await ctx.respond(error_message, ephemeral=True)
-
-@bot.slash_command(guild_ids=guild_ids)
-async def sell_shares(ctx, company_name: str, shares: int, price: float):
-    error_message = await api.sell_shares(ctx.user.id, company_name, shares, price)
-    if error_message is None:
-        await ctx.respond(f"{shares} shares of {company_name} have been put up for sale for {price}cc.", ephemeral=True)
-    else:
-        await ctx.respond(error_message, ephemeral=True)
-
-@bot.slash_command(guild_ids=guild_ids)
-async def buy_shares(ctx, company_name: str, shares: int, price: float):
-    error_message = await api.buy_shares(ctx.user.id, company_name, shares, price)
-    if error_message is None:
-        await ctx.respond(f"A buy order for {shares} shares at {price}cc/share of {company_name} has been posted.", ephemeral=True)
-    else:
-        await ctx.respond(error_message, ephemeral=True)
-
-@bot.slash_command(guild_ids=guild_ids)
-async def list_orders(ctx, company_name: str):
-        await ctx.respond(f"# {company_name}", ephemeral=True, view=ui.OrderListType(company=company_name))
-
-@bot.slash_command(guild_ids=guild_ids)
-async def company(ctx, company_name: str):
-    if True: # Check a user is a share holder
-        await ctx.respond(f"# {company_name}", ephemeral=True, view=ui.ShareholderCompanyView(company=company_name))
-    else:
-        await ctx.respond(f"# {company_name}", ephemeral=True, view=ui.CompanyView(company=company_name))
+# @bot.slash_command(guild_ids=guild_ids)
+# async def create_company(ctx, name: str, shares: int):
+#     error_message = await api.create_company(ctx.user.id, name, shares)
+#     if error_message is None:
+#         await ctx.respond(f"{name} has been created and {shares} shares have been issued.", ephemeral=True)
+#     else:
+#         await ctx.respond(error_message, ephemeral=True)
+# 
+# @bot.slash_command(guild_ids=guild_ids)
+# async def sell_shares(ctx, company_name: str, shares: int, price: float):
+#     error_message = await api.sell_shares(ctx.user.id, company_name, shares, price)
+#     if error_message is None:
+#         await ctx.respond(f"{shares} shares of {company_name} have been put up for sale for {price}cc.", ephemeral=True)
+#     else:
+#         await ctx.respond(error_message, ephemeral=True)
+# 
+# @bot.slash_command(guild_ids=guild_ids)
+# async def buy_shares(ctx, company_name: str, shares: int, price: float):
+#     error_message = await api.buy_shares(ctx.user.id, company_name, shares, price)
+#     if error_message is None:
+#         await ctx.respond(f"A buy order for {shares} shares at {price}cc/share of {company_name} has been posted.", ephemeral=True)
+#     else:
+#         await ctx.respond(error_message, ephemeral=True)
+# 
+# @bot.slash_command(guild_ids=guild_ids)
+# async def list_orders(ctx, company_name: str):
+#         await ctx.respond(f"# {company_name}", ephemeral=True, view=ui.OrderListType(company=company_name))
+# 
+# @bot.slash_command(guild_ids=guild_ids)
+# async def company(ctx, company_name: str):
+#     if True: # Check a user is a share holder
+#         await ctx.respond(f"# {company_name}", ephemeral=True, view=ui.ShareholderCompanyView(company=company_name))
+#     else:
+#         await ctx.respond(f"# {company_name}", ephemeral=True, view=ui.CompanyView(company=company_name))
 
 @bot.slash_command(guild_ids=guild_ids)
 async def create_token(ctx):
